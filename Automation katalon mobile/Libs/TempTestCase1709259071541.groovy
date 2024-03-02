adbb41ef61a733350685cd02eab2876ac8a44a6a
2yantihutasoit <yantihutasoit020@gmail.com>
2024-03-02 12:16:07 +0700

    first commit

---
+++ Automation katalon mobile/Libs/TempTestCase1709259071541.groovy
@@ -0,0 +1,26 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+import com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner
+import com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('C:\\Users\\GO\\AppData\\Local\\Temp\\Katalon\\Test Cases\\password salah\\20240301_091111\\execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/password salah', new TestCaseBinding('Test Cases/password salah',[:]), FailureHandling.STOP_ON_FAILURE , false)
+    
---
+++ Automation katalon mobile/Scripts/login dengan field email dikosongkan/Script1709260280734.groovy
@@ -0,0 +1,38 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\GO\\Downloads\\Diary with lock_4.96_apkcombo.com.apk', true)
+
+Mobile.swipe(1000, 800, 500, 300)
+
+Mobile.tap(findTestObject('Object Repository/login dengan field email dikosongkan/android.widget.Button - Sign in'), 0)
+
+Mobile.tap(findTestObject('Object Repository/login dengan field email dikosongkan/android.widget.Button - SIGN IN TO EXISTING ACCOUNT'), 
+    0)
+
+Mobile.setText(findTestObject('Object Repository/login dengan field email dikosongkan/android.widget.EditText - Password'), 
+    'Tintam2233', 0)
+
+Mobile.tap(findTestObject('Object Repository/login dengan field email dikosongkan/android.widget.Button - SIGN IN (1)'), 
+    0)
+
+Mobile.tap(findTestObject('Object Repository/login dengan field email dikosongkan/android.widget.EditText - Email'), 0)
+
+Mobile.closeApplication()
+
